e923452314821cbd834cffd4d4b6a3f20c28c3ec
1cedimi <dmitri.kalinin7@gmail.com>
2020-01-02 00:13:04 +0200

    Most frequent words; RSS Feed added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DOMDocument;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,46 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+      function count_words_quantity($text, $excludedWords, $limit)
+      {
+        $text = strip_tags(html_entity_decode($text)); //exclude tags
+        $text = strtolower($text); // Make all text to lowercase
+        $words = str_word_count($text, 1); // Returns an array containing all the words found inside the string
+        $words = array_diff($words, $excludedWords); // compare array and remove excluded words
+        $words = array_count_values($words); // Count words quantity
+        arsort($words); // Sort based on count
+        
+        return array_slice($words, 1, $limit); // Limit the number of words and returns the word array
+      }
+
+      $totalSummary = "";
+      $wordsQuantity = 10;
+      $commonWords = "the,be,to,of,and,a,in,that,have,I,it,for,not,on,with,he,as,you,do,at,this,but,his,by,from,they,we,say,her,she,or,an,will,my,one,all,would,there,their,what,so,up,out,if,about,who,get,which,go,me";
+      $commonWords = strtolower($commonWords);
+      $common_words_array = explode (",", $commonWords);  //create array of most common words
+    
+      $rssObject = new DOMDocument();
+      $rssObject->load("https://www.theregister.co.uk/software/headlines.atom"); //load Rss object
+      $entries = $rssObject->getElementsByTagName("entry");
+      
+      foreach($entries as $data){
+        /* $id = $data->getElementsByTagName("id")->item(0)->nodeValue; 
+        $updated = $data->getElementsByTagName("updated")->item(0)->nodeValue;
+        $link = $data->getElementsByTagName("link")->item(0)->nodeValue;
+        $author = $data->getElementsByTagName("author")->item(0)->nodeValue;
+        $uri = $data->getElementsByTagName("uri")->item(0)->nodeValue;
+        $name = $data->getElementsByTagName("name")->item(0)->nodeValue;
+        $title = $data->getElementsByTagName("title")->item(0)->nodeValue; */
+        $summary = $data->getElementsByTagName("summary")->item(0)->nodeValue;
+    
+        $totalSummary =  $totalSummary . " " . $summary;
+      }
+      $result = count_words_quantity($totalSummary, $common_words_array, $wordsQuantity);
+
+      return view('home')->with('result', $result)
+                         /* ->with('entries', $entries); */
+                         ->with(['entries'=>$entries]);
+                        
     }
+    
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+  <div class="row justify-content-center">
+    <div class="col-md-8">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+      <div class="card">
+        <div class="card-header mt-3">10 most frequent words
         </div>
+
+        <div class="card-body">
+          <ol>
+          @foreach($result as $word => $quantity) 
+            <li>) <b>{{$word}}</b> (appeared<b> {{$quantity}}</b> times)</li>
+          @endforeach
+          </ol>
+        </div>      
+      </div>
+
+      <div class="row justify-content-md-center breadcrumb mt-5">
+        <h1>RSS Feed</h1>
+      </div>
+
+      @foreach($entries as $data)      
+        <div class="card mt-3">      
+          <div class="card-header">
+            {!!$title = $data->getElementsByTagName("title")->item(0)->nodeValue!!}
+          </div>
+          <div class="card-body">
+            <label>Author:</label> {!!$name = $data->getElementsByTagName("name")->item(0)->nodeValue!!}
+            {!!$summary = $data->getElementsByTagName("summary")->item(0)->nodeValue!!}
+          </div>
+        </div>
+      @endforeach
+
+      
+
     </div>
+  </div>
 </div>
 @endsection
